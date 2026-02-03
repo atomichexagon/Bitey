ce40182ccc6c1682c18fb7452149a2f3b9e1f38a
Spookynutz <ppaduchowski@gmail.com>
2026-02-02 23:56:38 -0500

    Removed deployment file.

--- deploy.bat
+++
@@ -1,5 +0,0 @@
-rmdir /s /q "%APPDATA%\factorio\mods\biter-pet
-robocopy "%HOMEPATH%\Desktop\biter-pet" "%APPDATA%\factorio\mods\biter-pet" /MIR /XO /XC /XN
-del "%APPDATA%\factorio\mods\biter-pet\deploy.bat"
-cls
-echo Synchronization complete!
